a6dabc936b47cbbd2ff3c2c8c1e402723f4ba50f
Abenezer Gebeyehu <Abenezersoccer@gmail.com>
2022-03-05 14:05:43 -0500

    fixed the first function and will work on the last function

--- cs465_hw2_subroutines.asm
+++ cs465_hw2_subroutines.asm
@@ -39,42 +39,43 @@
 .globl atoi
 atoi:
 	li $t0, 0              # set the index to 0 for do while loop
-	li $t5, 0              # set sum to 0 for integer calcualtion
-	li $s4, 7		       # set value power of the highest order bit for 16^? calculation
-	li $s1, '/'		       # set the immediate value of $s1 to represent '/' which comes before '0'
-	li $t3, ':'		       # set the immediate value of $t3 to represent ':' which comes after '9'
-	li $a1, 9
-	la $t1, 0($a0)		       # load the base address our input of hex value into $t1
-	sub $a1, $a1, 1 
+	li $t1, 0              # set sum to 0 for integer calcualtion
+	li $t2, 7		       # set value power of the highest order bit for 16^? calculation
+	li $t3, '/'		       # set the immediate value of $s1 to represent '/' which comes before '0'
+	li $t4, ':'		       # set the immediate value of $t3 to represent ':' which comes after '9'
+	li $a1, 8
+	add $t5, $zero,  $a0	 # load the base address our input of hex value into $t1
     do_while:  			      
-    add $t2, $t1, $t0	       # we add $t0 which is our index to our base address
-    lb $t4, 0($t2)		       	# we then lb to load byte character into $s0 
-	slt $t6, $s1, $t4             # we check if the value of $s0 is greater than $s1('/')which is ascii value interm of integer as 47
-	slt $t7, $t4, $t3	       # we check if the value of $s0 is less than $t3 (':') which is ascii value interm of integer as 58
+    	add $s1, $t5, $t0	          # we add $t0 which is our index to our base address
+    	lb  $s2, 0($s1)		       	  # we then lb to load byte character into $s0 
+	slt $t6, $t3, $s2            # we check if the value of $s0 is greater than $s1('/')which is ascii value interm of integer as 47
+	slt $t7, $s2, $t4	          # we check if the value of $s0 is less than $t3 (':') which is ascii value interm of integer as 58
 	beq $t6, $0, checkoption2     # if  slt yield zero we do our secound check from A to F
 	beq $t7, $0, checkoption2     # if  slt yield zero we do our secound check from A to F
-	sub $t4, $t4, 48	       # after it passes the check, we subtract 48 to get the actual integer value
-	mul  $s5, $s4, 4		# we know based on the multiplicative property we can have 2^(4*2) to represent 16^2 so here we mulitply 4* the value of $s4
-	sllv $t4,$t4, $s5		# we then do left shift by variable to do the actual integervalue*2^(value *4)
-	add $t5, $t5, $t4		# we then add to sum+=value
-	j Incr			       # we jump to incr 
+	sub $s2, $s2, 48	          # after it passes the check, we subtract 48 to get the actual integer value
+	mul $s5, $t2, 4		      # we know based on the multiplicative property we can have 2^(4*2) to represent 16^2 so here we mulitply 4* the value of $s4
+	sllv $s2,$s2, $s5		      # we then do left shift by variable to do the actual integervalue*2^(value *4)
+	add $t1, $t1, $s2		      # we then add to sum+=value
+	j Incr			              # we jump to incr 
     checkoption2:	
-    sub $s0, $s0, 55
-    mul  $s5, $s4, 4		# we know based on the multiplicative property  of powers we can have 2^(4*2) to represent 16^2
-	sllv $t4,$t4, $s5	        # we are going to use the multipied power to use for shifting to the left
-	add $t5, $t5, $t4
+    	sub $s2, $s2, 55
+    	mul  $s5, $t2, 4		      # we know based on the multiplicative property  of powers we can have 2^(4*2) to represent 16^2
+	sllv $s2,$s2, $s5	          # we are going to use the multipied power to use for shifting to the left
+	add $t1, $t1, $s2
     Incr:
-    add $t0, $t0, 1 	  # index++
-    sub  $s4, $s4,1		  # subtract one from $s4 which hold the highest power 16^? value
+    	add $t0, $t0, 1 	  # index++
+    	sub  $t2, $t2,1		  # subtract one from $s4 which hold the highest power 16^? value
 	slt  $s6, $t0, $a1       # index < 9 jump while
 	bne  $s6, $0, do_while
-	addi $sp, $sp, -4
-	sw  $ra, 0($sp)
+	move $v0,$t1
 	li $a0, 1
+	addi $sp, $sp -8
+	sw $v0, 4($sp)
+	sw $ra, 0($sp)
 	jal report
+	lw $v0,  4($sp)
 	lw  $ra, 0($sp)
-	add $sp, $sp, 4
-	move $v0, $t5
+	add $sp, $sp, 8
 	jr $ra
 
 
@@ -150,11 +151,7 @@ get_type:
 	
 .globl get_dest_reg
 get_dest_reg:
-	#prepare return
-	#call report() to update status
-	jal report
-	lw  $t2,0($a0)		# load 32-bit unsigned number from atoi to $t0
-	srl $t1,$t2,26		# shift right logical 26 bits to get value for the opcode
+	srl $t1,$a0,26		# shift right logical 26 bits to get value for the opcode
 	beq $t1,$0, r_type	# if opcode == 0, goto r_type
 	li $t3,0x2		# $t3 = 2
 	beq $t1,$t3,no_reg	# if opcode == 2, goto no_reg
@@ -169,10 +166,18 @@ get_dest_reg:
 	j invalid		# else, goto invalid
 	
 cont:
+	li $a0, 3
+	addi $sp, $sp -8
+	sw $v0, 4($sp)
+	sw $ra, 0($sp)
+	jal report
+	lw $v0,  4($sp)
+	lw  $ra, 0($sp)
+	add $sp, $sp, 8
 	jr $ra
 	
 no_reg:
-	li $t1, 0x20		# return 32 if no register get updated
+	li $v0, 0x20		# return 32 if no register get updated
 	j cont
 	
 r_type:
@@ -190,18 +195,18 @@ rd:
 	srl $t1,$t2,11		# shift right logical 11 bits
 	li $t3, 0x1F		# create a mask to get 5-bit rd
 	and $t1,$t2,$t3		# masking 32-bit unsigned number and 0x1F to get 5-bit rd
-	move $a0, $t1		# return dest_reg number 
+	move $v0, $t1		# return dest_reg number 
 	j cont
 
 rt:
 	srl $t1,$t2,16		# shift right logical 16 bits 
 	li $t3, 0x1F		# create a mask to get 5-bit rt
 	and $t1,$t2,$t3		# masking 32-bit unsigned number and 0x1F to get 5-bit rt
-	move $a0, $t1		# return dest_reg number 
+	move $v0, $t1		# return dest_reg number 
 	j cont
 
 invalid:
-	li $t1, 0xFFFFFFFF	# return invalid
+	li $v0, 0xFFFFFFFF	# return invalid
 	j cont
 
 #############################################################
